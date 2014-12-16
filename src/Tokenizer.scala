933739905b014bf740f9a6dd51c141fdc0a8e3b5
rmellema <rene.mellema@gmail.com>
2014-12-16 23:33:09 +0100

    Added the tokenizer for strings

---
+++ src/Tokenizer.scala
@@ -0,0 +1,21 @@
+object Tokenizer {
+  def tokenize(s: String): List[String] = {
+    if (s.isEmpty) {
+      List()
+    } else {
+      val c: Char = s.head
+      val t = s.tail
+      if (c.isLetter) {
+        c +: t.takeWhile(_.isLetter) :: tokenize(t.dropWhile(_.isLetter))
+      } else if (c.isDigit) {
+        c +: t.takeWhile(_.isDigit) :: tokenize(t.dropWhile(_.isDigit))
+      } else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')') {
+        c.toString :: tokenize(t)
+      } else if (c.isSpaceChar) {
+        tokenize(t.dropWhile(_.isSpaceChar))
+      } else {
+        sys.error("Illegal character '" + c + "' in input")
+      }
+    }
+  }
+}
