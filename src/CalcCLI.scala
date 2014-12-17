28a59bde2911df1fac14a991314380a14755afbb
rmellema <rene.mellema@gmail.com>
2014-12-17 16:23:27 +0100

    Made the calculator closeble with :quit and ctrl+d

--- src/CalcCLI.scala
+++ src/CalcCLI.scala
@@ -2,10 +2,14 @@ object CalcCLI {
   def main(args: Array[String]): Unit = {
     println("Welcome to the calculator!")
     var ln = scala.io.StdIn.readLine("> ")
+    if (ln == null) {
+      ln = ":quit"
+      println(ln)
+    }
     var e: Expr = Val(0.0)
     var v = Map[String, Double]()
     var res: Double = 0.0
-    while (ln.length > 0) {
+    while (ln.take(2) != ":q") {
       try {
         e = Parser.parse(ln)
         res = e match {
@@ -20,6 +24,10 @@ object CalcCLI {
         case e: Exception => println(e.getMessage)
       }
       ln = scala.io.StdIn.readLine("> ")
+      if (ln == null) {
+        ln = ":quit"
+        println(ln)
+      }
     }
   }
 }
