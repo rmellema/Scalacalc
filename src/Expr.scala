563778e444ef6cc8facbea9eb89c1a61a2a233e0
rmellema <rene.mellema@gmail.com>
2014-12-17 14:51:48 +0100

    Added the eval method to each of the case classes

--- src/Expr.scala
+++ src/Expr.scala
@@ -1,51 +1,51 @@
-abstract class Expr{
+abstract class Expr {
   type Valuation = Map[String, Double]
-  def vars() :Array[String] = this match {
-    case Val(d) => Array()
-    case Var(s) => Array(s)
-    case Ass(n, e) => n +: e.vars()
-    case Sum(l, r) => l.vars().union(r.vars())
-    case Sub(l, r) => l.vars().union(r.vars())
-    case Mul(l, r) => l.vars().union(r.vars())
-    case Div(l, r) => l.vars().union(r.vars())
-    case Mod(l, r) => l.vars().union(r.vars())
-  }
 
-  def eval (v :Valuation) :Double = this match {
-    case Val(d) => d
-    case Var(s) => v.get(s) match {
-      case Some(r) => r
-      case _       => sys.error("Variable '" + s + "' not found!")
-    }
-    case Sum(l, r) => l.eval(v) + r.eval(v)
-    case Sub(l, r) => l.eval(v) - r.eval(v)
-    case Mul(l, r) => l.eval(v) * r.eval(v)
-    case Div(l, r) => l.eval(v) / r.eval(v)
-    case Mod(l, r) => l.eval(v) % r.eval(v)
-  }
+  def vars: Array[String]
+
+  def eval(v: Valuation): Double
 }
 
 case class Val(d: Double) extends Expr {
-   override def toString = d.toString
+  override def toString = d.toString
+  override def vars = Array()
+  override def eval(v:Valuation) = d
 }
 case class Var(s: String) extends Expr {
   override def toString = s
+  override def vars = Array(s)
+  override def eval(v: Valuation) = v.get(s) match {
+    case Some(d) => d
+    case _       => sys.error("Variable '" + s + "' not found in valuation")
+  }
 }
 case class Sum(l: Expr, r:Expr) extends Expr {
   override def toString = "(" + l.toString + " + " + r.toString + ")"
+  override def vars = l.vars.union(r.vars)
+  override def eval(v: Valuation) = l.eval(v) + r.eval(v)
 }
 case class Sub(l: Expr, r:Expr) extends Expr {
   override def toString = "(" + l.toString + " - " + r.toString + ")"
+  override def vars = l.vars.union(r.vars)
+  override def eval(v: Valuation) = l.eval(v) - r.eval(v)
 }
 case class Mul(l: Expr, r:Expr) extends Expr {
   override def toString = "(" + l.toString + " * " + r.toString + ")"
+  override def vars = l.vars.union(r.vars)
+  override def eval(v: Valuation) = l.eval(v) * r.eval(v)
 }
 case class Div(l: Expr, r:Expr) extends Expr {
   override def toString = "(" + l.toString + " / " + r.toString + ")"
+  override def vars = l.vars.union(r.vars)
+  override def eval(v: Valuation) = l.eval(v) / r.eval(v)
 }
 case class Mod(l: Expr, r:Expr) extends Expr {
   override def toString = "(" + l.toString + " % " + r.toString + ")"
+  override def vars = l.vars.union(r.vars)
+  override def eval(v: Valuation) = l.eval(v) % r.eval(v)
 }
 case class Ass(n: String, r:Expr) extends Expr {
   override def toString = "(" + n + " = " + r.toString+")"
+  override def vars = n +: r.vars
+  override def eval(v: Valuation) = r.eval(v)
 }
