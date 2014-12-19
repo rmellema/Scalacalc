dfdc9fe42ff567ce35d68ba48b716eab884ce251
rmellema <rene.mellema@gmail.com>
2014-12-19 23:38:51 +0100

    Finished the GUI except for size

--- src/CalcFrame.scala
+++ src/CalcFrame.scala
@@ -3,35 +3,33 @@ import java.awt.event.{ActionEvent, ActionListener}
 import javax.swing.{JTextField, JScrollPane, JTextArea, JFrame}
 
 object CalcFrame extends JFrame with ActionListener {
-  private var textArea:  JTextArea  = null
-  private var textField: JTextField = null
-  private var entered:   String     = ""
+  private val textArea:  JTextArea  = new JTextArea("Welcome to Scalacalc!\n", 25, 80)
+  private val textField: JTextField = new JTextField(80)
+  private val calc                  = new Calculator()
 
   private def write(s: String): Unit = {
     textArea.append(s + "\n")
   }
 
   override def actionPerformed(actionEvent: ActionEvent): Unit = {
-    entered = textField.getText
-    write(entered)
+    val ln = textField.getText
+    write("> " + ln)
     textField.setText("")
-    println("Read text: " + entered)
-  }
-
-  private def read (s: String): Unit = {
-
+    try {
+      write(calc.evaluate(ln).toString)
+    } catch {
+      case e: Exception => write(e.getMessage)
+    }
   }
 
   def initialize() = {
     setTitle("Scalacalc")
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-    textArea = new JTextArea("Welcome to Scalacalc!\n", 25, 80)
     textArea.setEditable(false)
     textArea.setLineWrap(true)
     textArea.setWrapStyleWord(true)
     val scroll = new JScrollPane(textArea)
     add(scroll, BorderLayout.CENTER)
-    textField = new JTextField(80)
     textField.addActionListener(this)
     add(textField, BorderLayout.SOUTH)
     setVisible(true)
