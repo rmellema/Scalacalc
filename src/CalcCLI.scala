b9b1f487f1c3ac0212b7c6e8fab6f6a415a41ce6
rmellema <rene.mellema@gmail.com>
2014-12-19 21:32:21 +0100

    Added a class to function as a Repl and made the CLI use that

--- src/CalcCLI.scala
+++ src/CalcCLI.scala
@@ -1,33 +1,6 @@
 object CalcCLI {
   def main(args: Array[String]): Unit = {
-    println("Welcome to the calculator!")
-    var ln = scala.io.StdIn.readLine("> ")
-    if (ln == null) {
-      ln = ":quit"
-      println(ln)
-    }
-    var e: Expr = Val(0.0)
-    var v = Map[String, Double]()
-    var res: Double = 0.0
-    while (ln.take(2) != ":q") {
-      try {
-        e = Parser.parse(ln)
-        res = e match {
-          case Ass(n, c) =>
-            val r = c.eval(v)
-            v = v + ((n, r))
-            r
-          case _ => e.eval(v)
-        }
-        println(res)
-      } catch {
-        case e: Exception => println(e.getMessage)
-      }
-      ln = scala.io.StdIn.readLine("> ")
-      if (ln == null) {
-        ln = ":quit"
-        println(ln)
-      }
-    }
+    println("Welcome to Scalacalc!")
+    (new CalcRepl((str) => io.StdIn.readLine(str), println)).run()
   }
 }
---
+++ src/CalcRepl.scala
@@ -0,0 +1,36 @@
+import scala.annotation.tailrec
+
+class CalcRepl(read: (String => String), write: (String => Unit)) {
+  @tailrec
+  final def run(v: Map[String, Double]): Unit = {
+    var newV = v
+    var ln = read("> ")
+    if (ln == null) {
+      ln = ":quit"
+      write(ln)
+    }
+    if (ln.take(2) == ":q") {
+      return
+    }
+
+    try {
+      val res = Parser parse ln match {
+        case Ass(n, e) =>
+          val r = e.eval(v)
+          newV = v + ((n, r))
+          r
+        case e         => e.eval(v)
+      }
+      write(res.toString)
+    } catch {
+      case e: Exception => println(e.getMessage)
+    }
+
+    run(newV)
+  }
+
+  def run(): Unit = {
+    run(Map[String, Double]())
+  }
+
+}
