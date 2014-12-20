c8befb76ddf4c96743fbc04bb6a90cfd9ec71a76
rmellema <rene.mellema@gmail.com>
2014-12-20 16:53:18 +0100

    Added rational type

--- src/edu/rmellema/Scalacalc/Number.scala
+++ src/edu/rmellema/Scalacalc/Number.scala
@@ -7,6 +7,9 @@ abstract class Number {
     case Real(d) =>
       if (d - d.floor == 0) Integer(d.toInt)
       else this
+    case Rational(n, d) =>
+      if (d == 1 || n % d == 0) Integer(n / d)
+      else this
     case _       => this
   }
 
@@ -45,7 +48,7 @@ case class Integer(i: scala.Int) extends Number {
     case Real   (r) => Real(i % r)
   }
   override def ^(n: Number): Number = n match {
-    case Integer(o) => Integer(i ^ o)
+    case Integer(o) => Integer(math.pow(i, o).toInt)
     case Real   (r) => Real(math.pow(i, r))
   }
 
@@ -85,4 +88,61 @@ case class Real(d: scala.Double) extends Number {
   override def toReal    = this
 
   override def toString  = d.toString
+}
+
+abstract case class Rational private(n: Int, d: Int) extends Number {
+  def copy(nom: Int = this.n, denom: Int = this.d) = Rational(nom, denom)
+  override def unary_- : Number = Rational(-n, d)
+
+  override def +(o: Number): Number = o match {
+    case Integer(i)     => Rational(n + d * i, d)
+    case Real(r)        => toReal + Real(r)
+    case Rational(m, f) => Rational(n * f + m * d, d * f)
+  }
+
+  override def -(o: Number): Number = o match {
+    case Integer(i)     => Rational(n - d * i, d)
+    case Real(r)        => toReal - Real(r)
+    case Rational(m, f) => Rational(n * f - m * d, d * f)
+  }
+
+  override def *(o: Number): Number =  o match {
+    case Integer(i)     => Rational(n * i, d)
+    case Real(r)        => Real(r) * toReal
+    case Rational(m, f) => Rational(n * m, d * f)
+  }
+
+  override def /(o: Number): Number = o match {
+    case Integer(i)     => Rational(n, d * i)
+    case Real(r)        => toReal / Real(r)
+    case Rational(m, f) => Rational(n * f, d * m)
+  }
+
+  override def ^(o: Number): Number = o match {
+    case Integer(i)     => Rational(math.pow(n, i).toInt, math.pow(d, i).toInt)
+    case Real(r)        => toReal ^ Real(r)
+    case Rational(m, f) => Rational(n * f, d * m)
+  }
+
+  override def %(o: Number): Number =
+    sys.error("Can't take the % of a Rational")
+
+  override def toInteger = toReal.toInteger
+  override def toReal    = Real(n/d)
+
+  override def toString  = n.toString + "/" + d.toString
+}
+
+object Rational {
+  private def gcd(x: Int, y: Int): Int = {
+    if (x == 0) y
+    else if (x < 0)  gcd(-x, y)
+    else if (y < 0) -gcd(x, -y)
+    else gcd(y % x, x)
+  }
+
+  def apply(n: Int, d: Int) = {
+    val g = gcd(n, d)
+    new Rational(n/g, d/g) {}
+  }
 }
