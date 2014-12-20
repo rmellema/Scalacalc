3fb5090f36d511f97ba35a0c6f317ed2f2f0966f
rmellema <rene.mellema@gmail.com>
2014-12-20 17:15:44 +0100

    Moved all of the number types into their seperate files

---
+++ src/edu/rmellema/Scalacalc/Integer.scala
@@ -0,0 +1,49 @@
+package edu.rmellema.Scalacalc
+
+case class Integer(i: scala.Int) extends Number {
+  override def unary_- = Integer(-i)
+
+  override def +(t: Number): Number = t match {
+    case Integer(o) => Integer(i + o)
+    case Real(r) => Real(i + r)
+    case Rational(n, d) => Rational(i * d + n, d)
+  }
+
+  override def -(t: Number): Number = t match {
+    case Integer(o) => Integer(i - o)
+    case Real(r) => Real(i - r)
+    case Rational(n, d) => Rational(i * d - n, d)
+  }
+
+  override def *(t: Number): Number = t match {
+    case Integer(o) => Integer(i * o)
+    case Real(r) => Real(i * r)
+    case Rational(n, d) => Rational(n * i, d).toNumber
+  }
+
+  override def /(t: Number): Number = t match {
+    case Integer(o) => Rational(i, o)
+    case Real(r) => Real(i / r).toNumber
+    case Rational(n, d) => Rational(n, d * i)
+  }
+
+  override def %(t: Number): Number = t match {
+    case Integer(o) => Integer(i % o)
+    case Real(r) => Real(i % r)
+    case Rational(n, d) => sys.error("Trying to % a Rational")
+  }
+
+  override def ^(t: Number): Number = t match {
+    case Integer(o) => Integer(math.pow(i, o).toInt)
+    case Real(r) => Real(math.pow(i, r))
+    case Rational(n, d) => Real(math.pow(i, n.toDouble / d))
+  }
+
+  override def toInteger = this
+
+  override def toReal = Real(i.toDouble)
+
+  override def toRational = Rational(i, 1)
+
+  override def toString = i.toString
+}
---
+++ src/edu/rmellema/Scalacalc/Real.scala
@@ -0,0 +1,41 @@
+package edu.rmellema.Scalacalc
+
+case class Real(d: scala.Double) extends Number {
+  override def unary_- = Real(-d)
+  override def +(t: Number): Number = t match {
+    case Integer(i) => Real(d + i)
+    case Real(o)    => Real(d + o).toNumber
+    case Rational(_, _) => this + t.toReal
+  }
+  override def -(t: Number): Number = t match {
+    case Integer(i) => Real(d - i)
+    case Real(o)    => Real(d - o).toNumber
+    case Rational(_, _) => this - t.toReal
+  }
+  override def *(t: Number): Number = t match {
+    case Integer(i) => Real(d * i).toNumber
+    case Real(o)    => Real(d * o).toNumber
+    case Rational(_, _) => this * t.toReal
+  }
+  override def /(t: Number): Number = t match {
+    case Integer(i) => Real(d / i).toNumber
+    case Real(o)    => Real(d / o).toNumber
+    case Rational(_, _) => this / t.toReal
+  }
+  override def %(t: Number): Number = t match {
+    case Integer(i) => Real(d % i).toNumber
+    case Real(o)    => Real(d % o).toNumber
+    case Rational(_, _) => this % t.toReal
+  }
+  override def ^(t: Number): Number = t match {
+    case Integer(i) => Real(math.pow(d, i)).toNumber
+    case Real(o)    => Real(math.pow(d, o)).toNumber
+    case Rational(_, _) => this ^ t.toReal
+  }
+  override def toInteger  = Integer(d.toInt)
+  override def toReal     = this
+  override def toRational = sys.error("Implement this!")
+
+  override def toString  = d.toString
+}
+
