b30b947614f38ecf0b479a53d19f19b404a6492e
rmellema <rene.mellema@gmail.com>
2016-07-28 21:35:28 +0200

    Made CLI commands a little nicer

--- src/edu/rmellema/Scalacalc/CalcCLI.scala
+++ src/edu/rmellema/Scalacalc/CalcCLI.scala
@@ -1,6 +1,19 @@
 package edu.rmellema.Scalacalc
 
 object CalcCLI {
+
+  def printVersion = {}
+
+  def printHelp = {
+    println("This is Scalacalc, a simple calculator written in Scala.")
+    println("Type in an arithmetic expression to have it evaluated. You can assign a value to a variable with `=`")
+    println("\tfoo = 42")
+    println("You can define functions using a similar syntax:")
+    println("\tsquare(x) = x * x")
+    println("The CLI understands commands starting with `:`. Currently implemented are:")
+    println("\thelp | h \t Print this help message")
+    println("\tquit | q \t Quit")
+  }
   def read(str: String) = {
     io.StdIn.readLine(str) match {
       case null =>
@@ -12,12 +25,20 @@ object CalcCLI {
 
   def run(calc: Calculator): Unit = {
     val ln = read("> ")
-    if (ln.take(2) == ":q") return
+    if (ln == ":q" || ln == ":quit") return
+    if (ln.take(1) == ":") {
+      ln.drop(1) match {
+        case "quit" | "q" => return
+        case "help" | "h" => printHelp
+        case "version" | "v" => printVersion
+      }
+    } else {
 
-    try {
-      println(calc.evaluate(ln))
-    } catch {
-      case e: Exception => println("Error: " + e.getMessage)
+      try {
+        println(calc.evaluate(ln))
+      } catch {
+        case e: Exception => println("Error: " + e.getMessage)
+      }
     }
     run(calc)
   }
